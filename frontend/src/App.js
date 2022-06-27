ec5b4d507a25ad11932a86e3367b22628e1b55d1
mangala-karunarathne <mangalaicc@gmail.com>
2022-06-27 10:30:45 -0700

    Frontend React App - Navigation Bar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,11 @@
 import './App.css';
 import CounterClass from './components/CounterClass';
 import CounterFunction from './components/CounterFunction';
-
+import Header from './components/Header';
 function App() {
   return (
-    <div className="App">
-      <h1> Hello Mangala welcome to the React World</h1>
-      <CounterClass/>
-      <CounterFunction/>
+    <div>
+      <Header/>
     </div>
   )
 }
