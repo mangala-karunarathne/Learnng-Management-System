da1d3fe5d3605f6f312e1cf1a5c374e8cd06a9a8
mangala-karunarathne <mangalaicc@gmail.com>
2022-06-27 23:14:29 -0700

    Both end Configered & DB Updated - Add Student

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ const dotenv = require("dotenv");
 const app = express();
 require("dotenv").config();
 
-const PORT = process.env.PORT || 8070;
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(bodyParser.json());
--- frontend/src/components/Addstudent.js
+++ frontend/src/components/Addstudent.js
@@ -1,17 +1,52 @@
 import React, {useState} from "react";
+import axios from "axios";
 
 
- function AddStudent(){
+function AddStudent(){
+
+    const[name,setName] = useState("");
+    const[age,setAge] = useState("");
+    const[gender,setGender] = useState("");
+
+    function sentData(e){
+
+        e.preventDefault();
+        const newStudent = {
+
+                name,
+                age,
+                gender
+
+        }
+
+       axios.post("http://localhost:5000/student/add",newStudent).then(()=>{
+           alert("Student Added");
+           setName("");
+           setAge("");
+           setGender("");
+
+
+       }).catch((err)=>{
+           alert(err)
+       })
+
+    }
+
     return(
 
         <div className="container">
-            <form>
+            <form onSubmit={sentData}>
                 
                 
                 <div class="from-group">
                     <label for="name" class="col-sm-2 col-form-label">Student Name</label>
                         <div >
-                        <input type="text" readonly class="form-control" id="name" placeholder="Enter Student Name"/>
+                        <input type="text" readonly class="form-control" id="name" placeholder="Enter Student Name"
+                        onChange={(e)=>{
+
+                            setName(e.target.value);
+
+                        }}/>
                         </div>
                 </div>
                 
@@ -19,29 +54,38 @@ import React, {useState} from "react";
                 <div class="from-group">
                     <label for="age" class="col-sm-2 col-form-label">Student Age</label>
                         <div >
-                        <input type="text" readonly class="form-control" id="age" placeholder="Enter Student Age"/>
+                        <input type="text" readonly class="form-control" id="age" placeholder="Enter Student Age"
+                        onChange={(e)=>{
+
+                            setAge(e.target.value);
+
+                        }}/>
                         </div>
                 </div>
                 
                 
                 <div class="from-group">
                     <label for="gender" class="col-sm-2 col-form-label">Gender</label>
-                    <div >
-                    <input type="text" class="form-control" id="gender" placeholder="Enter Student Gender"/>
-                    </div>
+                        <div >
+                        <input type="text" class="form-control" id="gender" placeholder="Enter Student Gender"
+                        onChange={(e)=>{
+
+                            setGender(e.target.value);
+
+                        }}/>
+                        </div>                    
                 </div>
                 
                 
-                <div class="from-group">
+                <div >
                     <button type="submit" class="btn btn-primary">Submit</button>
                 </div>
 
-                
+
             </form>
         </div>
             
     )
 }
-
 
 export default AddStudent;
