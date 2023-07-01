e1fc2c1615075672405e5efdff416570e1f2fa82
mangala-karunarathne <mangalaicc@gmail.com>
2023-07-01 09:00:07 +0530

    CHG | Small Changes

--- backend/routes/students.js
+++ backend/routes/students.js
@@ -1,84 +1,97 @@
-const router = require('express').Router();
-const { request } = require('express');
-let Student = require('../models/Student');
+const router = require("express").Router();
+const { request } = require("express");
+let Student = require("../models/Student");
 
-router.route('/add').post((req,res)=>{ // http://localhost:8070/student/add
-// Create in CRUD
-    const name = req.body.name;
-    const age = Number(req.body.age);
-    const gender = req.body.gender;
+router.route("/add").post((req, res) => {
+  // http://localhost:8070/student/add
+  // Create in CRUD
+  const name = req.body.name;
+  const age = Number(req.body.age);
+  const gender = req.body.gender;
 
-    const newStudent = new Student({
+  const newStudent = new Student({
+    name, // initialize
+    age,
+    gender,
+  });
 
-        name, // initialize
-        age,
-        gender
-
+  newStudent
+    .save()
+    .then(() => {
+      // Js Promise
+      res.status(200).json({
+        student,
+        message: "Student Added Successfully",
+        status: "success",
+      });
     })
+    .catch(() => {
+      console.log(err);
+    });
+});
 
-    newStudent.save().then(()=> {    // Js Promise
-        res.json('Student Added')
-
-    }).catch(()=>{
-        console.log(err);
+router.route("/").get((req, res) => {
+  // Read in CRUD
+  Student.find()
+    .then((students) => {
+      res.json(students);
     })
+    .catch((err) => {
+      console.log(err);
+    });
+});
 
-})
+router.route("/update/:id").put(async (req, res) => {
+  // Update in CRUD
+  let userId = req.params.id;
+  const { name, age, gender } = req.body; // Destructure
 
-router.route('/').get((req,res)=>{
-// Read in CRUD
-    Student.find().then((students)=>{
-        res.json(students)
-    }).catch((err) =>{
-        console.log(err)
-    })
+  const updateStudent = {
+    name,
+    age,
+    gender,
+  };
 
-})
-
-router.route('/update/:id').put(async (req, res) =>{
-// Update in CRUD
-    let userId = req.params.id;
-    const {name, age, gender} = req.body;// Destructure
-    
-    const updateStudent = {
-        name,
-        age,
-        gender
-    }
-
-    const update = await Student.findByIdAndUpdate(userId, updateStudent)
-     /* same thing can be done using following code,
+  const update = await Student.findByIdAndUpdate(userId, updateStudent)
+    /* same thing can be done using following code,
     const update = await Student.findByIdAndUpdate(userId, {name,age,gender})*/
-    .then(()=>{
-        res.status(200).send({status: "User updated"})
-    }).catch((err)=>{
-        console.log(err);
-        res.status(500).send({status: "Error with updating Data"});
+    .then(() => {
+      res.status(200).send({ status: "User updated" });
     })
-}) // back end userId fetch
+    .catch((err) => {
+      console.log(err);
+      res.status(500).send({ status: "Error with updating Data" });
+    });
+}); // back end userId fetch
 
 router.route("/delete/:id").delete(async (req, res) => {
-    // Delete in CRUD
-    let userId = req.params.id;
-    await Student.findByIdAndDelete(userId)
-    .then(() =>{
-        res.status(200).send({status: "User deleted"});
-    }).catch((err)=>{
-        console.log(err.message);
-        res.status(500).send({status: "Error with delete user", error: err.message});
+  // Delete in CRUD
+  let userId = req.params.id;
+  await Student.findByIdAndDelete(userId)
+    .then(() => {
+      res.status(200).send({ status: "User deleted" });
     })
-})
+    .catch((err) => {
+      console.log(err.message);
+      res
+        .status(500)
+        .send({ status: "Error with delete user", error: err.message });
+    });
+});
 
-router.route("/get/:id").get(async (req, res) =>{
-    // taking a student detail 
-    let userId = req.params.id;
-    const user = await Student.findById(userId) // await Student.findOne(email) >>> to get an email of a student ( as per requirement)
-        .then((student)=>{
-            const user = res.status(200).send({status:"User fetched", student})
-        }).catch((err) =>{
-             console.log(err.message);
-             res.status(500).send({status: "Error with get user", error: err.message});
-        })
-})
+router.route("/get/:id").get(async (req, res) => {
+  // taking a student detail
+  let userId = req.params.id;
+  const user = await Student.findById(userId) // await Student.findOne(email) >>> to get an email of a student ( as per requirement)
+    .then((student) => {
+      const user = res.status(200).send({ status: "User fetched", student });
+    })
+    .catch((err) => {
+      console.log(err.message);
+      res
+        .status(500)
+        .send({ status: "Error with get user", error: err.message });
+    });
+});
 
-module.exports = router;   
+module.exports = router;
--- frontend/src/components/AllStudents.js
+++ frontend/src/components/AllStudents.js
@@ -1,131 +1,180 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import "../Assets/Styles.css";
-import {VIEW_MODE,EDIT_MODE} from "./constant";
-
+import { VIEW_MODE, EDIT_MODE } from "./constant";
 
 export default function AllStudent() {
+  const [students, setStudents] = useState([]);
+  const [mode, setMode] = useState(VIEW_MODE);
+  const [id, setId] = useState("");
+  const [name, setName] = useState("");
+  const [age, setAge] = useState("");
+  const [gender, setGender] = useState("");
 
-const [students, setStudents] = useState([]);
-const [mode, setMode] = useState(VIEW_MODE);
-const [id, setId] = useState('');
-const[name,setName] = useState('');
-const[age,setAge] = useState('');  
-const [gender, setGender] = useState('');
+  /* To get all student details when render */
+  useEffect(() => {
+    axios
+      .get("http://localhost:5000/student/")
+      .then((res) => {
+        setStudents(res.data);
+        // console.log('Student Details: ', res.data);
+        // console.log('This is the response',res.data);
+      })
+      .catch((err) => {
+        alert(err.message);
+      });
+  }, []);
 
-/* To get all student details when render */
-useEffect(() => {
-        axios.get("http://localhost:5000/student/").then((res) => {
-            setStudents(res.data);
-            // console.log('Student Details: ', res.data);
-            // console.log('This is the response',res.data);
-            
-        }).catch((err) => {
-            alert(err.message);
+  /* To delete a student */
+  const deleteStudent = (id) => {
+    axios
+      .delete(`http://localhost:5000/student//delete/${id}`)
+      .then((res) => {
+        alert("Student Deleted Successfully");
+      })
+      .catch((err) => {
+        alert(err.message);
+      });
+
+    //load db data after delete a student
+    setTimeout(() => {
+      axios
+        .get("http://localhost:5000/student/")
+        .then((res) => {
+          setStudents(res.data);
         })
-    },[])
+        .catch((err) => {
+          alert(err.message);
+        });
+    }, 500);
+  };
 
-    /* To delete a student */
-    const deleteStudent = (id) =>{
-        axios.delete(`http://localhost:5000/student//delete/${id}`).then((res) => {
-            alert('Student Deleted Successfully');
-        }).catch((err) => {
-            alert(err.message);
+  const activeEditMode = (selectedId) => {
+    setMode(EDIT_MODE);
+    setId(selectedId);
+  };
+
+  const editStudent = (stdntid) => {
+    const newStudent = { name, age, gender };
+
+    axios
+      .put(`http://localhost:5000/student/update/${stdntid}`, newStudent)
+      .then((res) => {
+        console.log(res.data);
+        alert("Student Updated Successfully");
+        setName("");
+        setAge("");
+        setGender("");
+      })
+      .catch((err) => {
+        alert(err);
+      });
+
+    setMode(VIEW_MODE);
+
+    //load db data after delete a student
+    setTimeout(() => {
+      axios
+        .get("http://localhost:5000/student/")
+        .then((res) => {
+          setStudents(res.data);
         })
-        
-        //load db data after delete a student
-        setTimeout(()=>{
-            axios.get("http://localhost:5000/student/").then((res) => {
-            setStudents(res.data);
-            }).catch((err) => {
-            alert(err.message);
-            })
-            },500)
-    }
+        .catch((err) => {
+          alert(err.message);
+        });
+    }, 500);
+  };
 
+  return (
+    <div>
+      <div className="topic">
+        <h1>All Students</h1>
+      </div>
 
-    const activeEditMode = (selectedId) => {
-        setMode(EDIT_MODE);
-        setId(selectedId);
-    }
+      <div>
+        <table id="allStudents">
+          <tr>
+            <th>Name</th>
+            <th>Age</th>
+            <th>Gender</th>
+            <th>Edit/Delete</th>
+          </tr>
+          {students.map((std) => {
+            return (
+              <tr key={std._id}>
+                {mode === EDIT_MODE && id === std._id ? (
+                  <>
+                    <td>
+                      {" "}
+                      <input
+                        type="text"
+                        value={name}
+                        onChange={(e) => setName(e.target.value)}
+                        placeholder={std.name}
+                      />
+                    </td>
+                    <td>
+                      {" "}
+                      <input
+                        type="numeric"
+                        value={age}
+                        onChange={(e) => setAge(e.target.value)}
+                        placeholder={std.age}
+                      />
+                    </td>
+                    <td>
+                      {" "}
+                      <input
+                        type="text"
+                        value={gender}
+                        onChange={(e) => setGender(e.target.value)}
+                        placeholder={std.gender}
+                      />
+                    </td>
+                  </>
+                ) : (
+                  <>
+                    <td>{std.name}</td>
+                    <td>{std.age}</td>
+                    <td>{std.gender}</td>
+                  </>
+                )}
 
-    const editStudent = (stdntid) => {
-
-        const newStudent = { name, age, gender }
-
-       axios.put(`http://localhost:5000/student/update/${stdntid}`,newStudent).then(()=>{
-            alert("Student Updated Successfully");
-             setName('');
-             setAge('');
-             setGender('');
-       }).catch((err)=>{
-           alert(err);
-       })
-
-       setMode(VIEW_MODE);
-
-        //load db data after delete a student
-        setTimeout(()=>{
-            axios.get("http://localhost:5000/student/").then((res) => {
-            setStudents(res.data);
-            }).catch((err) => {
-            alert(err.message);
-            })
-            },500)
-    }
-    
-
-    return (
-    
-        <div>
-            <div className="topic">
-                <h1>All Students</h1>
-            </div>
-
-            <div>
-            <table id="allStudents">
-                <tr>
-                    <th>Name</th>
-                    <th>Age</th>
-                    <th>Gender</th>
-                    <th>Edit/Delete</th>
-                </tr>
-                     {students.map((std) => {
-                        return <tr key={std._id}>
-                             {mode === EDIT_MODE && id === std._id? (
-                                            <>
-                                             <td> <input type='text' value={name} onChange={e => setName(e.target.value)} placeholder={ std.name }/></td>
-                                            <td> <input type='numeric' value={age} onChange={e => setAge(e.target.value)} placeholder={ std.age }/></td>
-                                            <td> <input type='text' value={gender} onChange={e => setGender(e.target.value)} placeholder={ std.gender }/></td>
-                                            </> 
-                                            ) : (
-                                            <>
-                                            <td>{std.name}</td>
-                                            <td>{std.age}</td>
-                                            <td>{std.gender}</td>
-                                            </>
-                                        )}
-                                    
-                                    <td className="edit-dlt">
-                                        {mode === EDIT_MODE && id === std._id? (
-                                        <button className="btn" onClick={() => editStudent(std._id)}> Save </button>
-                                        ) : (
-                                            <>
-                                            <button className="btn" onClick={() => activeEditMode(std._id)}> Edit </button>
-                                            &nbsp;&nbsp;
-                                            {/* To make the space between buttons */}
-                                            <button className="btn" onClick={() => deleteStudent(std._id)}> Delete </button>
-                                            </>
-                                        )}
-                                        
-                                       
-                                       
-                                    </td>
-                               </tr>;
-                                    })
-                     }
-            </table>
-            </div>
-        </div>
-    )
+                <td className="edit-dlt">
+                  {mode === EDIT_MODE && id === std._id ? (
+                    <button
+                      className="btn"
+                      onClick={() => editStudent(std._id)}
+                    >
+                      {" "}
+                      Save{" "}
+                    </button>
+                  ) : (
+                    <>
+                      <button
+                        className="btn"
+                        onClick={() => activeEditMode(std._id)}
+                      >
+                        {" "}
+                        Edit{" "}
+                      </button>
+                      &nbsp;&nbsp;
+                      {/* To make the space between buttons */}
+                      <button
+                        className="btn"
+                        onClick={() => deleteStudent(std._id)}
+                      >
+                        {" "}
+                        Delete{" "}
+                      </button>
+                    </>
+                  )}
+                </td>
+              </tr>
+            );
+          })}
+        </table>
+      </div>
+    </div>
+  );
 }
